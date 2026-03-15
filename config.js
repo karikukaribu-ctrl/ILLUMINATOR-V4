c38396937fa66eae96c70f73820ec22294f3ee60
karikukaribu-ctrl <kariku.karibu@gmail.com>
2026-03-15 12:57:12 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+
+googleCalendar:{
+
+clientId:"YOUR_CLIENT_ID",
+
+apiKey:"YOUR_API_KEY",
+
+discoveryDocs:["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+
+scope:"https://www.googleapis.com/auth/calendar.readonly"
+
+}
+
+}
